5cac049c138e8e3cd2a297e57333b2ff2261e3f2
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-06-07 11:41:04 +0300

    Home_Work_11/6

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Solution(1);
+
+void Solution(int task)
+{
+    switch (task)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+
+    }
+}
