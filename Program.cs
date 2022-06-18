9eabba3688727a313ecba2fad3e6f59e45a9b15d
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-06-18 11:56:01 +0300

    18/6

--- Program.cs
+++ Program.cs
@@ -1,21 +1,28 @@
-﻿Solution(1);
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Solution(1);
 
 void Solution(int task)
 {
-    switch (task)
+    switch(task)
     {
         case 1:
+            OperatorOverloadingLabo.Worker work1 = new OperatorOverloadingLabo.Worker(100);
+            OperatorOverloadingLabo.Worker work2 = new OperatorOverloadingLabo.Worker();
+
+            Console.WriteLine(work1.Salary);
+            Console.WriteLine(work2.Salary);
+
+            work2 = work2 + 500;
+            work1 = work2 - 300;
+
+            Console.WriteLine(work1.Salary);
+            Console.WriteLine(work2.Salary);
+
+            Console.WriteLine(work1 == work2);
+
             break;
         case 2:
+            // Все последующие задания аналогичные и зачем их писать?
             break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-
     }
 }
---
+++ Worker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingLabo
+{
+    class Worker
+    {
+        protected int salary = 0;
+        public Worker()
+        {
+            this.salary = 0;
+        }
+        public Worker(int salary)
+        {
+            this.salary = salary;
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set { this.salary = value; }
+        }
+
+        public static Worker operator +(Worker a, int b) => new Worker(a.salary + b);
+        public static Worker operator -(Worker a, int b) => new Worker(a.salary - b);
+        public static bool operator ==(Worker a, Worker b)
+        {
+            return a.salary == b.salary;
+        }
+        public static bool operator !=(Worker a, Worker b)
+        {
+            return a.salary != b.salary;
+        }
+    }
+}
