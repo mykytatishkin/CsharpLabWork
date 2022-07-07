8a9f8b96fd519beda7a69d248a3e25e010c3cf9f
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-07-07 22:18:45 +0300

    7/07

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill", "Nikita", "Andrey" };
+            
+            var selectedPeople = from p in people
+                                 where p.ToUpper().StartsWith("T")
+                                 orderby p
+                                 select p;
+
+            foreach (string person in selectedPeople) 
+                Console.WriteLine(person);
+
+        }
+    }
+}
