a01413bb744e799e4726334d8362190ffe9f043b
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-09-01 15:58:36 +0300

    beg

---
+++ DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Создать массив типа ArrayList, наполните разными данными (int, string и тп.) и отсортируйте каждый тип с помощью пользовательского интерфейса IComparer
+             */
+        }
+    }
+}
