29d672188ef7ff5b8b5ed52053e9d161e4c059ee
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-09-02 11:30:09 +0300

    beg

---
+++ DZ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. Создать массив типа ArrayList, наполните разными данными (int, decimal, float и тп.) и отсортируйте каждый тип с помощью пользовательского интерфейса IComparer
+             * 2. Создайте generic класс Очередь с приоритетами. Реализуйте стандартные методы и свойства для работы очереди с приоритетами. (пример https://en.wikipedia.org/wiki/Priority_queue)
+             */
+
+            ArrayList array1 = new ArrayList();
+        }
+    }
+}
