d47554a6eff3def8217d96a749d35f199d74a052
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-09-02 11:06:26 +0300

    final

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +12,32 @@ namespace DZ
         {
             /*
              * Создать массив типа ArrayList, наполните разными данными (int, string и тп.) и отсортируйте каждый тип с помощью пользовательского интерфейса IComparer
+             * 
+             * https://docs.microsoft.com/en-us/dotnet/api/system.collections.arraylist?view=net-6.0
+             * 
+             * https://docs.microsoft.com/en-us/dotnet/api/system.collections.icomparer?view=net-6.0
              */
+
+            ArrayList myArr = new ArrayList();
+
+            myArr.Add("Hello");
+            myArr.Add("Generic");
+            myArr.Add("!");
+            PrintArr(myArr);
+
+            string[] words = { "vu", "vgtu", "c#", ".net", "asp.net", ".net core"};
+            PrintArr(words);
+
+            Array.Sort(words);
+            PrintArr(words);
+        }
+
+        public static void PrintArr(IEnumerable list)
+        {
+            foreach(var item in list)
+                Console.WriteLine(item);
+
+            Console.WriteLine();
         }
     }
 }
