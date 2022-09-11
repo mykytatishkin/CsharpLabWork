0fed7c255ec68e5be64c66d8226ac5bb82af6fa5
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-09-11 14:16:15 +0300

    fin

---
+++ DZ_1209/ArrayTools.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace DZ_1209
+{
+	public class ArrayTools
+    {
+
+		public void evenOutput(decimal[] array)
+		{
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Console.Write("\tEVEN: ");
+                    Console.Write(array[i] + " ");
+                }
+            }
+            Console.WriteLine("\nEnd of EVEN nums");
+		}
+		public void oddOutput(decimal[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    Console.Write("\tODD: ");
+                    Console.Write(array[i] + " ");
+                }
+            }
+            Console.WriteLine("\nEnd of ODD nums");
+        }
+		public void primeOutput(decimal[] array)
+        {
+            int j, p;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                j = 2;
+                p = 1;
+                while(j < array[i])
+                {
+                    if (array[i] % j == 0)
+                    {
+                        p = 0;
+                        break;
+                    }
+                    j++;
+                }
+
+                if(p == 1)
+                {
+                    Console.Write("\tPRIME: ");
+                    Console.Write(array[i] + " ");
+                }
+            }
+
+            Console.WriteLine("\nEnd of PRIME nums");
+        }
+		public void fibonachiOutput(decimal[] array)
+        {
+            bool resultFibonachi = true;
+
+            int urNum = array.Length;
+            // Console.Write("Enter num: "); urNum = int.Parse(Console.ReadLine());
+
+            int fibonachiElement = 0;
+            int fibonachiElementFirst = 0;
+            int fibonachiElementSecond = 1;
+
+
+            while (resultFibonachi)
+            {
+
+                fibonachiElement = fibonachiElementFirst + fibonachiElementSecond;
+                fibonachiElementFirst = fibonachiElementSecond;
+                fibonachiElementSecond = fibonachiElement;
+                Console.WriteLine(fibonachiElement);
+
+                if (urNum > fibonachiElement)
+                {
+                    resultFibonachi = false;
+                }
+            }
+            Console.WriteLine("\nFIBONACHI: {0}", resultFibonachi);
+        }
+	}
+}
+
---
+++ DZ_1209/Program.cs
@@ -0,0 +1,32 @@
+﻿using DZ_1209;
+using static System.Console;
+
+namespace DC_1209
+{
+    public delegate void ArrayToolBox(decimal[] array);
+
+    class Program
+    {
+        static public void Main()
+        {
+            decimal[] arrForTools = new decimal[10];
+            ArrayTools arrTool = new ArrayTools();
+
+            ArrayToolBox arrToolBox = new(arrTool.evenOutput);
+
+            arrToolBox += arrTool.oddOutput;
+            arrToolBox += arrTool.primeOutput;
+            arrToolBox += arrTool.fibonachiOutput;
+
+            // WriteLine("TEST_1");
+            for (int x = 0; x < 10; x++)
+            {
+                // WriteLine("TEST_2");
+                arrForTools[x] = (new Random()).Next(100);
+                // Console.Write(arrForTools[x] + " ");
+            }
+
+            arrToolBox(arrForTools);
+        }
+    }
+}
