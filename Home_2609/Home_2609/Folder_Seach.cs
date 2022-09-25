b6b7198faa71be86c55fa7a9e6122eb0183d5c03
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-09-25 15:04:46 +0300

    fiin

---
+++ Home_2609/Home_2609/Folder_Seach.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using static System.Console;
+namespace Home_2609
+{
+    public class Folder_Seach
+    {
+        // К сожалению у меня 1 диск и я на маке, так что не могу указать директорию которая была данна в задании.
+        protected string pathFolder = $"/Users/mykytatishkin/Projects/Home_2609/Folder_Search/";
+        protected string pathFilter;
+
+        public Folder_Seach(string pathFilter)
+        {
+            this.pathFilter = pathFilter;
+        }
+
+        public void FileSearchInMainDirectory()
+        {
+            string[] files = Directory.GetFiles(pathFolder, pathFilter);
+            foreach(var file in files)
+                WriteLine(file);
+        }
+        public void FileSearchInNotMainDirectory()
+        {
+            string[] directories = Directory.GetDirectories(pathFolder, pathFilter);
+            for(int i = 0; i < directories.Length; i++)
+            {
+                string[] files = Directory.GetFiles(pathFolder + directories[i], pathFilter);
+                foreach(var file in files)
+                    WriteLine(file);
+            }
+        }
+        public void FileDeletingInMainDirectory()
+        {
+            string[] files = Directory.GetFiles(pathFolder, pathFilter);
+            foreach (var file in files)
+                File.Delete(file);
+        }
+        public void FileDeletingInNotMainDirectory()
+        {
+            string[] directories = Directory.GetDirectories(pathFolder, pathFilter);
+            for (int i = 0; i < directories.Length; i++)
+            {
+                string[] files = Directory.GetFiles(pathFolder + directories[i], pathFilter);
+                foreach (var file in files)
+                    File.Delete(file);
+            }
+        }
+        public void FileDeletingInNotMainDirectoryOfDirectory()
+        {
+            string[] directories = Directory.GetDirectories(pathFolder, pathFilter);
+            for (int i = 0; i < directories.Length; i++)
+            {
+                string[] directories2 = Directory.GetFiles(pathFolder + directories[i], pathFilter);
+                for(int j = 0; j < directories2.Length; j++)
+                {
+                    string[] files = Directory.GetFiles(pathFolder + directories2[i], pathFilter);
+                    foreach (var file in files)
+                        File.Delete(file);
+                }
+            }
+        }
+        public void task_6()
+        {
+            string[] files = Directory.GetFiles(pathFolder, pathFilter);
+            string[] directories = Directory.GetDirectories(pathFolder, pathFilter);
+            foreach (var file in files)
+                WriteLine(file);
+
+            foreach (var dir in directories)
+                WriteLine(dir);
+        }
+    }
+}
+
---
+++ Home_2609/Home_2609/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace Home_2609
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Folder_Seach fs = new Folder_Seach("*.txt");
+
+            fs.FileSearchInMainDirectory();
+        }
+    }
+}
