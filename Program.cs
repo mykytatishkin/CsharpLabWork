03d354e7d44b160a87a5da1d10fdc8bad7759fac
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-10-02 13:02:17 +0300

    fin

--- Program.cs
+++ Program.cs
@@ -1,21 +1,50 @@
-﻿Solution(1);
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-void Solution(int task)
+namespace ConsoleDateParser
 {
-    switch (task)
+    internal class Program
     {
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
+        static void Main(string[] args)
+        {
+            /*
+             Создать регулярное выражение для извлечения даты, без учета знаков разделителя. 
+             Вывести результат на отдельные части: дата, месяц, год
+             */
 
+            string date = @"(?<data>[1-3][0-9])[\.\/\-\s\\](?<month>[0-9]{2})[\.\/\-\s\\](?<year>[0-9]{4})\b";
+
+            string BirthDate = "25/01/2005";
+
+            var regDate = new Regex(date);
+
+            if (regDate.IsMatch(BirthDate))
+            {
+                try
+                {
+                    int year;
+                    int.TryParse(regDate.Match(BirthDate).Groups["year"].Value, out year);
+
+                    int month;
+                    int.TryParse(regDate.Match(BirthDate).Groups["month"].Value, out month);
+
+                    int day;
+                    int.TryParse(regDate.Match(BirthDate).Groups["day"].Value, out day);
+
+                    DateTime dtBirth = new DateTime(year, month, day);
+
+                    Console.WriteLine("BirthDate :" + dtBirth.ToString("dd-MM-yyy"));
+                }
+
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+        }
     }
+}
 }
