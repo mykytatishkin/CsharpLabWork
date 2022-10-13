9d90d414937a97c626a4b80100c68d22238fb979
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-10-13 18:56:04 +0300

    sc

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsDiary
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDiary = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstDiary
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.lstDiary, 3);
+            this.lstDiary.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstDiary.FormattingEnabled = true;
+            this.lstDiary.Location = new System.Drawing.Point(3, 3);
+            this.lstDiary.Name = "lstDiary";
+            this.lstDiary.Size = new System.Drawing.Size(378, 372);
+            this.lstDiary.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.lstDiary, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnAdd, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnEdit, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnDelete, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(384, 418);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAdd.Location = new System.Drawing.Point(3, 381);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(121, 34);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnEdit.Location = new System.Drawing.Point(130, 381);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(122, 34);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Редактировать";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDelete.Location = new System.Drawing.Point(258, 381);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 34);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 418);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ежедневник";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDiary;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using WindowsFormsDiary.Model;
+
+namespace WindowsFormsDiary
+{
+    public partial class FrmMain : Form
+    {
+        BindingList<DiaryItem> Items = new BindingList<DiaryItem>();
+
+        public FrmMain()
+        {            
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            /*ДЗ: К существующей форме редактора задач, добавить поле напоминания типа дата и время
+             * запускать оповещение в момент наступления события
+             */
+
+            lstDiary.DataSource = Items;
+            lstDiary.DisplayMember = "FullName";
+            lstDiary.ValueMember = "Id";
+
+            if (File.Exists("./file.json")) 
+            {
+                var json = File.ReadAllText("./file.json");
+
+                if (!string.IsNullOrEmpty(json))
+                {
+                    foreach (var item in JsonConvert.DeserializeObject<BindingList<DiaryItem>>(json))
+                    {
+                        Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(Items);
+
+                File.WriteAllText("./file.json", json);
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "Diary", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (FrmEditor frm = new FrmEditor()) 
+            {
+                if (frm.ShowDialog(this) == DialogResult.OK) 
+                {
+                    Items.Add(new DiaryItem() { Id = Items.Count + 1, Name = frm.DiaryName, Created = DateTime.Now });
+                }
+            }                
+        }
+
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            var item = lstDiary.SelectedItem as DiaryItem;
+
+            using (FrmEditor frm = new FrmEditor())
+            {
+                frm.DiaryName = item.Name;
+
+                if (frm.ShowDialog(this) == DialogResult.OK)
+                {
+                    item.Name = frm.DiaryName;
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var item = lstDiary.SelectedItem as DiaryItem;
+
+            if (item != null)
+                Items.Remove(item);
+        }
+    }
+}
