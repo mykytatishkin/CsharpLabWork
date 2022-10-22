1167a2a25b4576a1538c0c84027ea7b4ae7358d9
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-10-22 19:49:30 +0300

    crash 3rd frm

--- FrmMain.cs
+++ FrmMain.cs
@@ -36,7 +36,7 @@ namespace WindowsFormsDiary
                     {
                         Items.Add(item);
                     }
-                }
+                } 
             }
         }
 
